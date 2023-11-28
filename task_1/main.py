b60b541672e0e328a70f78b14dc4d486d45f8888
Piotr Grzybowski <piotr.grzybowski@baitholders.com>
2023-11-28 09:52:38 +0100

    Prepare tasks

--- task_1/main.py
+++ task_1/main.py
@@ -1,42 +1,27 @@
+"""
+Tasks:
+- Inspection:
+  - Load the dataset into a Pandas DataFrame.
+  - Display the first 5 rows of the DataFrame.
+  - Convert the Date column to a DateTime type.
+
+- Analysis:
+  - Find unique Products names
+  - Calculate and display the total sales revenue (Price * Quantity) for the dataset.
+  - Add new column named code that contains the last 3 characters of the Product name.
+  - Find the average price of products sold.
+  - Group the data by Product and calculate the total quantity sold for each product.
+"""
+
 from pathlib import Path
 import pandas as pd
 
-def load_and_inspect_data(path: Path) -> pd.DataFrame:
-    pass
-
-def format_data(df: pd.DataFrame) -> pd.DataFrame:
-    pass
-
-def calculate_total_sales(df: pd.DataFrame):
-    pass
-
-def calculate_average_price(df: pd.DataFrame):
-    pass
-
-def find_highest_sales_product(df: pd.DataFrame):
-    pass
-
-def total_quantity_per_product(df: pd.DataFrame):
-    pass
-
-def most_popular_payment_type(df: pd.DataFrame):
-    pass
 
 def main():
     file_name = Path('sales_data.csv')
-    df = load_and_inspect_data(file_name)
+    df = pd.read_csv(file_name)
+    print(df)
 
-    total_revenue = calculate_total_sales(df)
-    average_price = calculate_average_price(df)
-    highest_sales_product = find_highest_sales_product(df)
-    quantity_per_product = total_quantity_per_product(df)
-    popular_payment_type = most_popular_payment_type(df)
-
-    print(f"Total Sales Revenue: {total_revenue}")
-    print(f"Average Price of Products: {average_price}")
-    print(f"Product with the Highest Sales Quantity: {highest_sales_product}")
-    print(f"Total Quantity Sold per Product:\n{quantity_per_product}")
-    print(f"Most Popular Payment Type: {popular_payment_type}")
 
 if __name__ == "__main__":
     main()
